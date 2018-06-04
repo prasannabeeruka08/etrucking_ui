ef8b4a40be8daad0c5fc6fc49eae593800dd9e2f
kranthi <kkumar.devjava@gmail.com>
2018-06-04 13:56:16 -0700

    undate Customer html and css, js

--- html/scripts/customers.js
+++ html/scripts/customers.js
@@ -8,8 +8,23 @@ $(document).ready(function(){
             $("#div1").html(result);
         }});
     });
+    var counter = 0;
     $("#AddNewContactDetails").click(function(){
-        var div = '<div class="col-sm-6"><div class="md-form-group"><input class="md-input" placeholder="Phone / Email"></div></div><div class="col-sm-6"><div class="md-form-group"><input class="md-input"></div></div>';
+        var div = '<div class="col-sm-6"><div class="md-form-group"><input class="md-input" id = "contact'+counter+'" placeholder="Phone / Email"></div></div><div class="col-sm-6"><div class="md-form-group"><input id = "details'+counter+'" class="md-input"></div></div>';
+        counter ++;
         $("#NewContactDetails").append(div);
+    });
+    $("#AddNewCustomer").click(function(){
+        var customerId = $("#customerId").val(),
+        customerName = $("#customerName").val(),
+        address = $("#address").val(),
+        city = $("#city").val(),
+        state = $("#state").val(),
+        zipcode = $("#zipcode").val(),
+        notes = $("#notes").val(),
+        phone1 = $("#phone1").val(),
+        phone2 = $("#phone2").val(),
+        email1 = $("#email1").val(),
+        email2 = $("#email2").val();
     });
 });
