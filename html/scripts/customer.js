8436da2d9caa3a06de38ba0be4a364a64ca3950b
kranthikandi <kranthikumar.kandi@gmail.com>
2018-10-29 12:35:01 -0700

    added new pages and updated datatable library

--- html/scripts/customer.js
+++ html/scripts/customer.js
@@ -1,3 +1,21 @@
 $(document).ready(function() {
-    $('#jobs').DataTable();
+
+
+    $.ajax({url: "http://localhost:3000/api/contacts", success: function(result){
+            alert(result);
+            console.log(result);
+            $.each(result, function(key, val){
+                $.each(val, function(k, vals){
+                    if (k== "first_name"){
+                        var fname = vals;
+                    }
+                    
+                console.log(fname);
+                })
+                
+            });
+            $("#div1").html(result);
+        }});
+
+    $("#driver_table").DataTable();
 } );
